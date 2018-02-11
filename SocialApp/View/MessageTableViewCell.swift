f2c31065cdbe8ba3dbfe7e559db8ddc85e9ba5d7
Bartosz Pawełczyk <bartoszpawelczyk@Mac-mini-Bartosz.local>
2018-02-11 19:11:29 +0100

    First version of HomeVC

---
+++ SocialApp/View/MessageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageTableViewCell.swift
+//  SocialApp
+//
+//  Created by Bartosz Pawełczyk on 11.02.2018.
+//  Copyright © 2018 Bartosz Pawełczyk. All rights reserved.
+//
+
+import UIKit
+
+class MessageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SocialApp/View/NewMessageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  NewMessageCollectionViewCell.swift
+//  SocialApp
+//
+//  Created by Bartosz Pawełczyk on 11.02.2018.
+//  Copyright © 2018 Bartosz Pawełczyk. All rights reserved.
+//
+
+import UIKit
+
+class NewMessageCollectionViewCell: UICollectionViewCell {
+    
+}
